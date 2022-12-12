b534ae2559bb51a018b76c79a61cb04ce7ebaa8b
sudoCss <mogo.gogo170@gmail.com>
2022-12-12 14:17:26 +0300

    For Hussam

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 #define WINDOW_TITLE "OpenGL Application"
 #define TIMER_MS 25
 
+#define SIMPLEBMP_OPENGL
+#include "simplebmp.h"
+
 void cleanup()
 {
 }
@@ -29,40 +32,7 @@ void handleKeypress(unsigned char key, int x, int y)
     }
 }
 
-void handleKeypressReleased(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-    case '\r': // Enter key
-        break;
-    case 27: // Escape key
-        cleanup();
-        exit(0);
-    }
-}
-
-void handleSpecialKeypress(int key, int x, int y)
-{
-    switch (key)
-    {
-    case GLUT_KEY_LEFT:
-        break;
-    case GLUT_KEY_RIGHT:
-        break;
-    }
-}
-
-void handleSpecialKeyReleased(int key, int x, int y)
-{
-    switch (key)
-    {
-    case GLUT_KEY_LEFT:
-        break;
-    case GLUT_KEY_RIGHT:
-        break;
-    }
-}
-
+GLuint textureId;
 void initRendering()
 {
     // Background color
@@ -72,10 +42,21 @@ void initRendering()
     glEnable(GL_DEPTH_TEST);                           // Enable 3D layering
     glEnable(GL_COLOR_MATERIAL);                       // Enable Better coloring
     glEnable(GL_LIGHTING);                             // Enable Lighting
+    glEnable(GL_LIGHT0);                               // Enable the first light
     glEnable(GL_NORMALIZE);                            // Enable Normalizing
     glEnable(GL_CULL_FACE);                            // Enable Cull face effect
+    glEnable(GL_TEXTURE_2D);                           // Enable textures
     glShadeModel(GL_SMOOTH);                           // Set Shading model to (Smooth)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // Set the blend function
+
+    SimpleBMP bmp;
+    int err = bmp.load("./assets/vtr.bmp");
+    if (err)
+        std::cout << "ERR" << std::endl;
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    bmp.glTexImage2D();
+    bmp.destroy();
 }
 
 void handleResize(int new_screen_width, int new_screen_height)
@@ -95,16 +76,28 @@ void drawScene()
     glMatrixMode(GL_MODELVIEW);                         // Switch to the drawing perspective
     glLoadIdentity();                                   // Reset the drawing perspective
 
-    // Code here
-    glTranslated(0, 0, -5);
+    GLfloat ambientLight[] = {0.2f, 0.2f, 0.2f, 1.0f};
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+
+    GLfloat directedLight[] = {0.7f, 0.7f, 0.7f, 1.0f};
+    GLfloat directedLightPos[] = {-10.0f, 15.0f, 20.0f, 0.0f};
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, directedLight);
+    glLightfv(GL_LIGHT0, GL_POSITION, directedLightPos);
+
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glColor3f(1.0f, 1.0f, 1.0f);
+
 
     glBegin(GL_TRIANGLES);
-    glColor3d(0.5, 0.8, 0.2);
-    glVertex2d(-0.5, -0.5);
-    glVertex2d(0.5, -0.5);
-    glVertex2d(0.0, 0.5);
-    glEnd();
-
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex3f(-2.5f, -2.5f, -8.5f);
+    glTexCoord2f(10.0f, 0.0f);
+    glVertex3f(2.5f, -2.5f, -8.5f);
+    glTexCoord2f(5.0f, 5.0f);
+    glVertex3f(0.0f, 2.5f, -8.5f);
     glEnd();
 
     glutSwapBuffers();
@@ -129,9 +122,6 @@ int main(int argc, char **argv)
 
     glutDisplayFunc(drawScene);
     glutKeyboardFunc(handleKeypress);
-    glutKeyboardUpFunc(handleKeypressReleased);
-    glutSpecialFunc(handleSpecialKeypress);
-    glutSpecialUpFunc(handleSpecialKeyReleased);
     glutReshapeFunc(handleResize);
     glutTimerFunc(TIMER_MS, update, 0);
 
